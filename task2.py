ae5e936fdc89f460375b28eb6052ddc5860d1d9c
Regina Shakirova <reginafar22@rambler.ru>
2022-12-19 00:55:19 +0400

    решила вторую задачу

--- task2.py
+++ task2.py
@@ -6,3 +6,19 @@
 #  длинаширинамасса асфальта для покрытия одного кв метра дороги асфальтом,
 #  толщиной в 1 см*число см толщины полотна. Проверить работу метода.
 # Например: 20м*5000м*25кг*5см = 12500 т
+
+class Road:
+
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+        self.weight_per_meter = 25
+        self.thickness = 5
+
+    def find_weight(self):
+        weight = self._length * self._width * self.weight_per_meter * self.thickness
+        print(weight)
+
+
+r = Road(5000, 20)
+r.find_weight()
